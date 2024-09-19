e540b3d31b52f69d61b502f1947f3d1757ff962a
Shibhi <shibhimurugu2004@gmail.com>
2024-09-19 12:11:30 +0530

    Editing GOI forms-1

--- src/GOI/registrationForm.js
+++ src/GOI/registrationForm.js
@@ -1,18 +1,17 @@
 import React from "react";
 import Navbar from "./NavBar";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import VehicleRegistrationForm from "./VehicleManagement/RegistrationForm";
 import UserRegistrationForm from "./UserManagement/UserRegistrationForm";
 
-
 function RegistrationForm() {
   return (
-    <div className="grid">
-      <div className="head g-col-12">
-        <Navbar />
-      </div>
-      <div className="container g-col-12">
-        <Router>
+    <Router>
+      <div className="grid">
+        <div className="head g-col-12">
+          <Navbar />
+        </div>
+        <div className="container g-col-12">
           <Routes>
             <Route
               path="/vehicle-registration"
@@ -23,9 +22,9 @@ function RegistrationForm() {
               element={<UserRegistrationForm />}
             />
           </Routes>
-        </Router>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
